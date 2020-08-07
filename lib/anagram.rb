942d4cf23c7077b6c68adf4766ed1c2076d8392e
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-07 20:05:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,7 @@ class Anagram
   end
 
   def match(word)
+<<<<<<< HEAD
    anagram = []
    arr = %w(words happy soccer need go show ab inlets oscar wild fan fast slow gallery regally largely)
    word.each do |ana|
@@ -22,5 +23,19 @@ class Anagram
      end
    end
     anagram.uniq
+=======
+   arr = %w(words happy soccer need go show banana oscar wild fan fast slow)
+   word.each do |ana|
+     arr.each do |gram|
+       ana = ana.split("!")
+       gram = gram.split("!")
+       binding.pry
+       if ana.sort == gram.sort 
+         binding.pry
+       end
+     end
+   end
+    
+>>>>>>> 3b41562af5da11e5cdafb57bfd8d4ca26af06263
   end
 end
